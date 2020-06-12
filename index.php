11f105dd25166428e2895b478a6f9ba9a45a08f8
Marc Totzke <Marc.Totzke@t-online.de>
2020-06-12 17:16:35 +0200

    MinorChanges to see development is working

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
  * Time: 19:24
  */
 
+$returnValue = apcu_add("someKey", "someValue");
+echo '<pre>' , var_dump($returnValue) , '</pre>';
+echo '<pre>' , var_dump(apcu_cache_info()) , '</pre>';
+$someValue = apcu_fetch("someKey");
+echo '<pre>' , var_dump($someValue) , '</pre>';
+
 $composerAutoLoader = require("./Packages/Libraries/autoload.php");
 $bootstrap = new \TheWorldsCMS\Journey\Core\Bootstrap($composerAutoLoader, "Development");
 $bootstrap->run();
